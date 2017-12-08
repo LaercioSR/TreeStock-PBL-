d8049bdd7b13223c92f006495093a5658d468c84
Laercio de Souza Rios <32028338+LaercioSR@users.noreply.github.com>
2017-12-07 22:21:24 -0300

    Update package-info.java

--- src/br/uefs/ecomp/treeStock/facade/package-info.java
+++ src/br/uefs/ecomp/treeStock/facade/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Pacaote contém a classe {@link br.uefs.ecomp.treeStock.facade.TreeStockFacade
+ * Pacote contÃ©m a classe {@link br.uefs.ecomp.treeStock.facade.TreeStockFacade
  * TreeStockFacade} que intermedia a {@link br.uefs.ecomp.treeStock.view.TreeStockView 
  * view} e o {@link br.uefs.ecomp.treeStock.controller.TreeStockController controller}
  */
